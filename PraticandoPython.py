093de7ac323fbabe9b830fc51b85041e4b4ebafb
Mauricio <mauricio.furlan@hotmail.com>
2019-09-14 00:15:51 -0300

    SomaSemMais
    Em andamento

---
+++ PraticandoPython.py
@@ -0,0 +1,41 @@
+
+def somar(valor1, valor2):
+    a = valor1
+    b = valor2
+    if (valor1 < 0 and valor2 > 0 or (valor1 > 0 and valor2 < 0)) and abs(a) == abs(b):
+        return 0
+    else:
+        try:
+            return a if b == 0 else somar(a ^ b, (a & b) << 1) if a < 0 else somar(b ^ a, (b & a) << 1)
+        except Exception:
+            a = ~valor1 + 1
+            b = ~valor2 + 1
+            try:
+                resultado = a if b == 0 else somar(a ^ b, (a & b) << 1) if a < 0 else somar(b ^ a, (b & a) << 1)
+                return resultado * (-1)
+            except Exception:
+                return '???????????'
+
+
+def somaNaMao():
+    valor1 = int(input('Valor1:'))
+    valor2 = int(input('Valor2:'))
+    resultado = somar(valor1, valor2)
+    print(f'\n{valor1} + {valor2} =  {resultado}')
+
+
+def somaAutomatica():
+    valor1 = 0
+    valor2 = 0
+    for i in range(100):
+        for j in range(10 + 1):
+            result = somar(valor1, valor2)
+            print(f'{valor1} + {valor2} = {result}')
+            valor2 = valor2 - 1
+        valor1 = valor1 - 1
+        valor2 = 0
+
+
+
+somaNaMao()
+somaAutomatica()
--- Somar.py
+++ Somar.py
@@ -7,7 +7,7 @@ print(somar(4,4))
     # 4 em binário = 00000100
     # 4 em binário = 00000100
 
-    # (valor1 & valor2) 4 & 4
+    # (valor1 & valor2) 4 & 4 O operador copiao bit para o resultado, se existir nos dois operandos
     # segue tabela &
     # 0 & 0 = 0
     # 0 & 1 = 0
@@ -17,7 +17,7 @@ print(somar(4,4))
     # 00000100
     #=00000100 = 4
 
-    # valor1 ^ valor 2 = 4 XOR 4
+    # valor1 ^ valor 2 = 4 XOR 4 Ele copia o bit se estiver definido em um operando, mas não em ambos.
     # segue tabela ^
 
     # 0 XOR 0 = 0
@@ -30,10 +30,10 @@ print(somar(4,4))
 
     #<< Binary Left Shift
 
-    #4 = 000000100
     #<<-------------
     #add left shift
-    #= 00001000 = 8
+    #4 000000100
+    #= 000001000 = 8
 
 
 
